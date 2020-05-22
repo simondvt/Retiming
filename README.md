0ccb2c1eb6e78b9f64a53b64cb8642595bbc879e
Simone De Vita <simonedevita1997@gmail.com>
2020-05-22 14:17:28 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Retiming
-Advanced Algorithms project on Retiming Graph
+# Retiming Graph
+### Advanced Algorithms project on Retiming Graph.
 
 Simone De Vita
 Matricola: 944021
+
+Content:
+- RetimingGraph.hpp: Header file
+- RetimingGraph.cpp: Implementation file
+- main.cpp: example
+- test.cpp: testing
+- RetimingGraphPPT.ppt: Powerpoint presentation
+- optimalCorrelator1.dot: Correlator1 after the algorithm
--- main.cpp
+++ main.cpp
@@ -1,39 +1,38 @@
-//#include <iostream>
-//using namespace std;
-//
-//#include "RetimingGraph.hpp"
-//
-//int main()
-//{
-//    RetimingGraph rg;
-//    // Correlator 1 (Figure 3, pag 5)
-//    rg.addVertex(0); // Vh
-//    rg.addVertex(3); // V1
-//    rg.addVertex(3); // V2
-//    rg.addVertex(3); // V3
-//    rg.addVertex(3); // V4
-//    rg.addVertex(7); // V5
-//    rg.addVertex(7); // V6
-//    rg.addVertex(7); // V7
-//    rg.addEdge(0, 1, 1);
-//    rg.addEdge(1, 2, 1);
-//    rg.addEdge(2, 3, 1);
-//    rg.addEdge(3, 4, 1);
-//    rg.addEdge(1, 7, 0);
-//    rg.addEdge(2, 6, 0);
-//    rg.addEdge(3, 5, 0);
-//    rg.addEdge(4, 5, 0);
-//    rg.addEdge(5, 6, 0);
-//    rg.addEdge(6, 7, 0);
-//    rg.addEdge(7, 0, 0);
-//
-//    rg.printGraph();
-//    cout << "CP: " << rg.CP() << endl;
-//    rg.OPT(RetimingGraph::optEnum::OPT2);
-//    cout << "Retimed CP: " << rg.CP() << endl;
-//    rg.printGraph();
-//    rg.saveDOT("optimalCorrelator1.dot");
-//    
-//
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+#include "RetimingGraph.hpp"
+
+int main()
+{
+    RetimingGraph rg;
+    // Correlator 1 (Figure 3, pag 5)
+    rg.addVertex(0); // Vh
+    rg.addVertex(3); // V1
+    rg.addVertex(3); // V2
+    rg.addVertex(3); // V3
+    rg.addVertex(3); // V4
+    rg.addVertex(7); // V5
+    rg.addVertex(7); // V6
+    rg.addVertex(7); // V7
+    rg.addEdge(0, 1, 1);
+    rg.addEdge(1, 2, 1);
+    rg.addEdge(2, 3, 1);
+    rg.addEdge(3, 4, 1);
+    rg.addEdge(1, 7, 0);
+    rg.addEdge(2, 6, 0);
+    rg.addEdge(3, 5, 0);
+    rg.addEdge(4, 5, 0);
+    rg.addEdge(5, 6, 0);
+    rg.addEdge(6, 7, 0);
+    rg.addEdge(7, 0, 0);
+
+    rg.printGraph();
+    cout << "CP: " << rg.CP() << endl;
+    rg.OPT(RetimingGraph::optEnum::OPT1);
+    cout << "Retimed CP: " << rg.CP() << endl;
+    rg.printGraph();
+    rg.saveDOT("optimalCorrelator1.dot");
+
+    return 0;
+}
